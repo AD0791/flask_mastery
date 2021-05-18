01f495b442f62071f0019cef9c013d041e2b4455
AD0791 <alexdisla1991@gmail.com>
2021-05-18 14:38:42 -0400

    homework 2 is done

--- debut/app.py
+++ debut/app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template("index.html",name="MR Davidson")
+def index():
+    return render_template("index.html",name="Alexandro Disla")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(port=1300,debug=True)
-
-
+ 
 
